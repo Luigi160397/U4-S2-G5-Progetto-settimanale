0921381c6c493f34688c25731240647c7d673577
Luigi160397 <luigi.difraia.97@gmail.com>
2023-05-12 16:10:31 +0200

    esercizio 5

--- src/application/Application.java
+++ src/application/Application.java
@@ -1,9 +1,13 @@
 package application;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.UUID;
+
+import org.apache.commons.io.FileUtils;
 
 import application.catalogo.Catalogo;
 import application.catalogo.Libro;
@@ -11,6 +15,8 @@ import application.catalogo.Periodicita;
 import application.catalogo.Rivista;
 
 public class Application {
+
+	private static File file = new File("archivio.txt");
 
 	public static void main(String[] args) {
 		List<Catalogo> listaCataloghi = new ArrayList<>();
@@ -35,35 +41,49 @@ public class Application {
 
 		Catalogo daCercare = cercaISBN(listaCataloghi, rivista1.getISBN());
 		if (daCercare != null) {
-			System.out.println("Libro/Rivista trovato/a per ISBN:");
+			System.out.println("----------------- Libro/Rivista trovato/a per ISBN: ----------------- ");
 			System.out.println(daCercare);
 
 		} else {
-			System.out.println("Libro/Rivista non trovato per ISBN");
+			System.out.println("----------------- Libro/Rivista non trovato per ISBN ----------------- ");
 		}
 
 		List<Catalogo> cercatoPerAnno = cercaPerAnno(listaCataloghi, 2020);
 
 		if (cercatoPerAnno.size() > 0) {
-			System.out.println("Libro/Rivista trovato/a per anno:");
+			System.out.println("----------------- Libro/Rivista trovato/a per anno: ----------------- ");
 			System.out.println(cercatoPerAnno);
 		} else {
-			System.out.println("Nessun Libro/Rivista trovato per anno.");
+			System.out.println("----------------- Nessun Libro/Rivista trovato per anno. ----------------- ");
 		}
 
 		List<Libro> cercatoPerAutore = cercaPerAutore(listaCataloghi, "JK Rowling");
 
 		if (cercatoPerAutore.size() > 0) {
 			System.out.println();
-			System.out.println("Libri trovati per autore:");
+			System.out.println("----------------- Libri trovati per autore: ----------------- ");
 			System.out.println(cercatoPerAutore);
 		} else {
-			System.out.println("Nessun libro trovato.");
+			System.out.println("----------------- Nessun libro trovato. ----------------- ");
+		}
+
+		try {
+			System.out.println();
+			System.out.println("----------------- Lista aggiornata da archivio: ----------------- ");
+			leggiArchivio(listaCataloghi);
+		} catch (IOException e) {
+			System.out.println("----------------- File non trovato! ----------------- ");
 		}
 
 	}
 
 	public static void aggiungiCatalogo(List<Catalogo> lista, Catalogo c1) {
+		try {
+			salvaInArchivio(c1);
+		} catch (IOException e) {
+
+			System.out.println("----------------- File non trovato ----------------- ");
+		}
 		lista.add(c1);
 	}
 
@@ -94,5 +114,33 @@ public class Application {
 				.filter(catalogo -> catalogo instanceof Libro && ((Libro) catalogo).getAutore().equals(autore))
 				.map(catalogo -> (Libro) catalogo).toList();
 		return listaFiltrata;
+	}
+
+	public static void salvaInArchivio(Catalogo c) throws IOException {
+		if (c instanceof Libro) {
+			String lineaLibro = c.getISBN() + "@" + c.getTitolo() + "@" + c.getAnnoPublicazione() + "@" + c.getnPagine()
+					+ "@" + ((Libro) c).getAutore() + "@" + ((Libro) c).getGenere() + "#";
+			FileUtils.writeStringToFile(file, lineaLibro + System.lineSeparator(), "UTF-8", true);
+		} else {
+			String lineaRivista = c.getISBN() + "@" + c.getTitolo() + "@" + c.getAnnoPublicazione() + "@"
+					+ c.getnPagine() + "@" + ((Rivista) c).getPeriodicita() + "#";
+			FileUtils.writeStringToFile(file, lineaRivista + System.lineSeparator(), "UTF-8", true);
+		}
+	}
+
+	public static void leggiArchivio(List<Catalogo> mediaList) throws IOException {
+		if (file.exists()) {
+			String content = FileUtils.readFileToString(file, "UTF-8");
+			String[] separatedItems = content.split("#");
+			for (String string : separatedItems) {
+				String[] separated = string.split("@");
+				for (String s : separated) {
+					System.out.println(s);
+				}
+			}
+		} else {
+			System.out.println("----------------- Nessun file presente ----------------- ");
+		}
+
 	}
 }
